f30845d21d33a066acfc7da6be99150f3b7c0fe8
Aliya Tang <aliyatang11@gmail.com>
2024-06-09 13:21:24 -0400

    finished coding the todo application + it works

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./About.css";
-import profile_pic_name from "../assets/profile_pic_name.jpg";
+import profile_pic_name from "../assets/profile_pic_name.jpeg";
   
 export default class About extends Component {
   render() {
@@ -17,7 +17,7 @@ export default class About extends Component {
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">ang</div>
+            <div className="name_title">Aliya Tang</div>
             <div className="brief_description">
               Hi everyone! My name is Aliya Tang, a rising junior at Barnard College studying Computer Science. I am interested 
               in computer vision and robotics. In the future, I hope to explore a role in Software Engineering!
